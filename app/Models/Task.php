fb789a1847f54d83afd7af7d6e5a16821493ddd1
Nitesh Devnath <110216353+nimisha666@users.noreply.github.com>
2025-03-02 13:43:05 +0530

    Initial commit

---
+++ app/Models/Task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TaskFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /** @use HasFactory<TaskFactory> */
+    use HasFactory;
+
+
+    protected $fillable = ['name'];
+}
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\API\V1\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CompeleteTaskController;
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+    Route::patch('tasks/{task}/complete', CompeleteTaskController::class);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
