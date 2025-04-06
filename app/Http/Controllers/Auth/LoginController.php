396fa6c54bb336c54018d4240bf1efae566486e6
Nitesh Devnath <42641530+blackvikings@users.noreply.github.com>
2025-04-06 14:06:18 +0530

    login changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,12 +15,19 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = User::where('email', $request->email)->first();
-        if (!$user || !Hash::check('password', $user->password)) {
+//        $user = User::where('email', $request->email)->first();
+//        if (!$user || !Hash::check('password', $user->password)) {
+//            throw ValidationException::withMessages([
+//                'email' => ['The provided credentials are incorrect.'],
+//                'password' => ['The provided credentials are incorrect.'],
+//            ]);
+//        }
+
+        if (!auth()->attempt($request->only(['email', 'password']))) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
-                'password' => ['The provided credentials are incorrect.'],
             ]);
         }
+
     }
 }
